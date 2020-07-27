926686488af594285e0327069517279285531712
diogoaraujo <diogo.araujo@twisper.com>
2020-07-27 14:17:18 +0100

    refactored code

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -6,3 +6,15 @@ func fibonacci(n int64) int64 {
 	return fibonacci(n -1) + fibonacci(n - 2)
 }
 
+//faster approach
+func fasterFibonacci(n int64) int64 {
+	if n == 0 {return 0}
+	var arr = make([]int64, n + 1)
+	arr[0], arr[1] = 0, 1
+
+	for i := int64(2); i <= n; i++ {
+		arr[i] = arr[i - 1] + arr[i - 2]
+	}
+	return arr[n]
+}
+
--- fibonacci/fibonacci_test.go
+++ fibonacci/fibonacci_test.go
@@ -1,1 +1,31 @@
 package fibonacci
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"testing"
+	"time"
+)
+
+type cenas func (n int64) int64
+
+func TestFibonacci(t *testing.T) {
+	timeForFunc(fibonacci)
+}
+
+func TestFasterFibonacci(t *testing.T) {
+	timeForFunc(fasterFibonacci)
+}
+
+func timeForFunc(c cenas) {
+	n := 60
+	start := time.Now()
+	c(int64(n))
+	elapsed := time.Since(start)
+	fmt.Printf("Function named %s, took %d miliseconds for n = %d \n ", GetFunctionName(c), elapsed, n)
+}
+
+func GetFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
